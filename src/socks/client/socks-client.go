9d7e55dc2f938d1df7943e0986e9d67f90206c7f
fuq <fuq@digitalchina.com.cn>
2025-08-04 09:47:28 +0800

    feat(socks): 实现简单的 TCP 代理客户端和服务器
    - 添加了 socks 客户端和服务器的实现
    - 客户端可以连接到服务器并发送消息- 服务器可以接收客户端消息并返回 echo
    - 实现了基本的 TCP 连接管理和错误处理

--- src/socks/client/socks-client.go
+++ src/socks/client/socks-client.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
 
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:12345")
+	if err != nil {
+		panic(err)
+	}
+	tcpConn := conn.(*net.TCPConn)
+	_ = tcpConn.SetKeepAlive(true)
+	defer conn.Close()
+
+	stdin := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("send > ")
+		text, _ := stdin.ReadString('\n')
+		text = strings.TrimSpace(text)
+		if strings.ToLower(text) == "q" {
+			return
+		}
+		if _, err := conn.Write([]byte(text + "\n")); err != nil {
+			fmt.Println("write error:", err)
+			return
+		}
+
+		resp, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("recv < " + resp)
+	}
 }
--- src/socks/server/socks-server.go
+++ src/socks/server/socks-server.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
 
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:12345")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("TCP server listening on :12345")
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("accept error:", err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			return // 客户端断开
+		}
+		fmt.Printf("recv: %s", msg)
+		conn.Write([]byte("echo: " + msg))
+	}
 }
